8c272e5850d404a35102016455a488e73ede8fe3
Rayno Fahru <fahrurrozi.r24u@gmail.com>
2026-08-04 14:27:25 +0700

    Final website KKN

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,10 @@
 import "./Hero.css";
 import merapi from "../assets/images/Gunung Merapi.jpeg";
+import { useNavigate } from "react-router-dom";
 
 export default function Hero() {
+  const navigate = useNavigate();
+
   return (
     <section
       className="hero"
@@ -10,33 +13,41 @@ export default function Hero() {
       }}
     >
       <div className="hero-overlay">
+        <div className="hero-content">
+          <span className="hero-tag">
+            WEBSITE RESMI PADUKUHAN PAGERJURANG
+          </span>
 
-        <span className="hero-tag">
-          WEBSITE RESMI PADUKUHAN PAGERJURANG
-        </span>
+          <h1>
+            Digitalisasi Profil
+            <br />
+            Padukuhan Pagerjurang,
+            <br />
+            Kalurahan Kepuharjo
+          </h1>
 
-        <h1>
-          Digitalisasi Profile
-          <br />
-          Padukuhan Pagerjurang, Kalurahan Kepuharjo
-        </h1>
+          <p>
+            Menyajikan informasi mengenai profil Padukuhan Pagerjurang,
+            UMKM lokal, potensi wisata, berita kegiatan masyarakat,
+            galeri dokumentasi, serta informasi kontak secara digital.
+          </p>
 
-        <p>
-          Menyajikan informasi mengenai profile Padukuhan Pagerjurang,
-          UMKM lokal, potensi wisata, berita kegiatan masyarakat,
-          galeri dokumentasi, serta informasi kontak secara digital.
-        </p>
+          <div className="hero-button">
+            <button
+              className="btn-primary"
+              onClick={() => navigate("/umkm")}
+            >
+              Jelajahi UMKM
+            </button>
 
-        <div className="hero-button">
-          <button className="btn-primary">
-            Jelajahi UMKM
-          </button>
-
-          <button className="btn-secondary">
-            Tentang Pagerjurang
-          </button>
+            <button
+              className="btn-secondary"
+              onClick={() => navigate("/profile")}
+            >
+              Tentang Pagerjurang
+            </button>
+          </div>
         </div>
-
       </div>
     </section>
   );
